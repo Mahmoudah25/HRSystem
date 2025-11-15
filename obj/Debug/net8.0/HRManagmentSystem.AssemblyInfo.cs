29c6db480fa5f454d3db191627b3dc06c1e85925
MahmoudAhm25 <mahmoudandelsamed@gmail.com>
2025-11-15 10:09:06 -0800

    Remove secret from code

--- obj/Debug/net8.0/HRManagmentSystem.AssemblyInfo.cs
+++ obj/Debug/net8.0/HRManagmentSystem.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HRManagmentSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c748e8352c0d0d5a5bd683b27fc9a6b68d2d0b8d")]
 [assembly: System.Reflection.AssemblyProductAttribute("HRManagmentSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HRManagmentSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
